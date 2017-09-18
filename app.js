aa233bab6bd004eb0eec0cd39dfb6393371ae5d6
Frederik Nordahl <frederiknordahl@gmail.com>
2017-09-18 13:16:47 +0200

    remaking the video, sort of !

--- app.js
+++ app.js
@@ -51,8 +51,23 @@ var video = document.querySelector('video');
 
 video.currentTime = 20;
 
-document.querySelector('.more').onclick = function() {
+
+
+var muteBtn = document.querySelector('.sound');
+var more = document.querySelector('.more');
+
+more.onclick = function() {
 	document.querySelector('body').classList.add('active');
+}
+
+muteBtn.onclick = function() {
+	if (muteBtn.classList.contains('active')) {
+		audio.volume = 1;
+		muteBtn.classList.remove('active');
+	} else {
+		audio.volume = 0;
+		muteBtn.classList.add('active');
+	}
 }
 
 document.querySelector('.back').onclick = function() {
@@ -62,6 +77,8 @@ document.querySelector('.back').onclick = function() {
 var audio = document.querySelector('audio');
 var loader = document.querySelector('.loader');
 
+
+audio.currentTime = 42;
 
 var time = 0;
 
@@ -75,11 +92,14 @@ var counting = false;
 //$load1Start: 2200ms;
 //$load2Start: 16500ms;
 
-var between = 2100;
-var startOne = 2200;
+var between = 2300;
+var startOne = 6200;
+var startTwo = 18000;
+var startThree = 34400;
 
 var blockOne = document.querySelector('.load:not(.load2)');
 var blockTwo = document.querySelector('.load2');
+var blockThree = document.querySelector('.info');
 
 var fromOne = 0;
 var startedOne = false;
@@ -87,10 +107,25 @@ var startedOne = false;
 var fromTwo = 0;
 var startedTwo = false;
 
-console.log(blockTwo);
+var fromThree = 0;
+var startedThree = false;
+
+var lastVidChange = 0;
+
+var ended = false;
+
+var startBlur = 4700;
 
 function tick(ms) {
 	time = parseInt(ms);
+
+	if (!counting && window.isReady && time > 3000) {
+		counting = true;
+		video.classList.add('active');
+		video.play();
+		audio.play();
+		loader.classList.add('hide');
+	}
 
 	if (counting && countFrom == 0) {
 		countFrom = time;
@@ -101,9 +136,22 @@ function tick(ms) {
 		fromOne = time;
 	}
 
-	if (startedOne) {
-		ourTime = time - fromOne;
+	if (counting && !startedTwo && time - countFrom >= startTwo) {
+		startedTwo = true;
+		fromTwo = time;
+	}
 
+	if (counting && !startedThree && time - countFrom >= startThree) {
+		startedThree = true;
+		fromThree = time;
+	}
+
+	if (counting && time - countFrom >= startBlur) {
+		video.classList.add('blur');
+	}
+
+	if (startedOne) {
+		var ourTime = time - fromOne;
 
 		if (ourTime <= between) {
 			blockOne.classList.add('active');
@@ -112,6 +160,7 @@ function tick(ms) {
 			blockOne.classList.remove('active');
 			blockOne.childNodes[1].classList.remove('active');
 			blockOne.childNodes[3].classList.add('active');
+			blockOne.childNodes[3].classList.add('blink');
 		} else if (ourTime >= between * 2 && ourTime <= between * 3) {
 			blockOne.classList.add('active');
 			blockOne.childNodes[3].classList.remove('active');
@@ -122,18 +171,86 @@ function tick(ms) {
 		}
 	}
 
+	if (startedTwo) {
+		var ourTime2 = time - fromTwo;
+
+		if (ourTime2 <= between) {
+			blockTwo.classList.add('active');
+			blockTwo.childNodes[1].classList.add('active');
+		} else if (ourTime2 >= between && ourTime2 <= between * 2) {
+			blockTwo.classList.remove('active');
+			blockTwo.childNodes[1].classList.remove('active');
+			blockTwo.childNodes[3].classList.add('active');
+		} else if (ourTime2 >= between * 2 && ourTime2 <= between * 3) {
+			blockTwo.classList.add('active');
+			blockTwo.childNodes[3].classList.remove('active');
+			blockTwo.childNodes[5].classList.add('active');
+			blockTwo.childNodes[5].classList.add('blink');
+
+			document.querySelector('.overlay').classList.add('yellow');
+		} else {
+			blockTwo.classList.remove('active');
+			blockTwo.childNodes[5].classList.remove('active');
+		}
+	}
+
+	if (startedThree) {
+		var ourTime3 = time - fromThree;
+
+		if (ourTime3 <= between) {
+			blockThree.childNodes[1].classList.add('active');
+		} else if (ourTime3 >= between && ourTime3 <= between * 2) {
+			blockThree.childNodes[1].classList.remove('active');
+			blockThree.childNodes[3].classList.add('active');
+
+		} else if (ourTime3 >= between * 2 && ourTime3 <= between * 3) {
+			blockThree.childNodes[3].classList.remove('active');
+			blockThree.childNodes[5].classList.add('active');
+
+		} else if (ourTime3 >= between * 3 && ourTime3 <= between * 4) {
+			blockThree.childNodes[5].classList.remove('active');
+			blockThree.childNodes[7].classList.add('active');
+
+		} else if (ourTime3 >= between * 4 && ourTime3 <= between * 5) {
+			blockThree.childNodes[7].classList.remove('active');
+			blockThree.childNodes[9].classList.add('active');
+
+		} else {
+			blockThree.childNodes[9].classList.remove('active');
+			more.classList.add('active');
+
+
+
+			if (!ended) {
+				video.classList.remove('active');
+				loader.classList.add('rerun');
+
+				var volume = 1;
+
+				var int = setInterval(function () {
+					volume -= .02;
+
+					if (volume < .02) {
+						volume = 0;
+						clearInterval(int);
+					}
+
+					audio.volume = volume;
+				}, 100);
+
+				setTimeout(function () {
+					video.pause();
+				}, 3000);
+
+				ended = true;
+			}
+		}
+	}
+
 	requestAnimationFrame(tick);
 }
 
 tick();
-
-setTimeout(function () {
-	counting = true;
-	video.classList.add('active');
-	video.play();
-	audio.play();
-	loader.classList.add('hide');
-}, 3000);
 
 var body = document.querySelector('body');
 var content = document.querySelector('.content');
